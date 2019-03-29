9bdf624c9cd13b16d15a0d445bfe325d998d6e63
izhostov <ivan.zhostov@foursoft.io>
2019-03-29 14:15:16 +0200

    people list with fake api

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { Person } from '../models/person';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private _people: ReplaySubject<Person []> ;
+
+  constructor() { }
+
+  public getPeople(): Person[] {
+    let person: Person[] = [
+      { firstname: 'Ivan', lastname: 'Ivanov', birthDate: new Date() },
+      { firstname: 'Georgi', lastname: 'Georgiev', birthDate: new Date() }
+    ];
+
+    return person;
+  }
+
+  public get people() {
+
+    this._people = new ReplaySubject<Person []>(1);
+    this._people.next(this.getPeople());
+
+    return this._people;
+  }
+}
